4849b158b679a5326ad099e80b73783cbf089866
angelsolaorbaiceta <angelsolaorbaiceta@gmail.com>
2018-02-04 13:25:13 +0100

    add method to know if matrix is row dominant

--- mat/matrices.go
+++ mat/matrices.go
@@ -1,6 +1,10 @@
 package mat
 
-import "github.com/angelsolaorbaiceta/inkmath"
+import (
+	"math"
+
+	"github.com/angelsolaorbaiceta/inkmath"
+)
 
 /*
 IsSquare returns true if the given matrix has the same number of rows and columns.
@@ -14,12 +18,34 @@ IsSymmetric returns true if the given matrix is square and equals to it's traspo
 */
 func IsSymmetric(m Matrixable) bool {
 	if !IsSquare(m) {
-		return false
+		panic("Matrix symeetry only applies to square matrices")
 	}
 
 	for i := 0; i < m.Rows(); i++ {
 		for j := i + 1; j < m.Cols(); j++ {
 			if !inkmath.FuzzyEqual(m.Value(i, j), m.Value(j, i)) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+/*
+IsRowDominant returns true if for every row in the matrix, the element in the main diagonal
+is greater than every other element.
+*/
+func IsRowDominant(m Matrixable) bool {
+	if !IsSquare(m) {
+		panic("Matrix dominancy only applies to square matrices")
+	}
+
+	var diagonalValue float64
+	for i := 0; i < m.Rows(); i++ {
+		diagonalValue = math.Abs(m.Value(i, i))
+		for j := 0; j < m.Cols(); j++ {
+			if i != j && diagonalValue < math.Abs(m.Value(i, j)) {
 				return false
 			}
 		}
--- mat/matrices_test.go
+++ mat/matrices_test.go
@@ -30,18 +30,36 @@ func TestIsSymmetric(t *testing.T) {
 	}
 }
 
-func TestNonSquareIsNotSymmetric(t *testing.T) {
-	m := MakeSparse(1, 3)
-	if IsSymmetric(m) {
-		t.Error("Expected matrix not to be symmetric")
-	}
-}
-
 func TestIsNotSymmetric(t *testing.T) {
 	m := MakeSparse(5, 5)
 	m.SetValue(3, 4, 9.0)
 
 	if IsSymmetric(m) {
 		t.Error("Expected matrix not to be symmetric")
+	}
+}
+
+/* <--------------- Dominant ---------------> */
+func TestIsRowDominant(t *testing.T) {
+	m := MakeSparse(2, 2)
+	m.SetValue(0, 0, 50.0)
+	m.SetValue(0, 1, 10.0)
+	m.SetValue(1, 1, -50.0)
+	m.SetValue(1, 0, 10.0)
+
+	if !IsRowDominant(m) {
+		t.Error("Expected matrix to be row dominant")
+	}
+}
+
+func TestIsNotRowDominant(t *testing.T) {
+	m := MakeSparse(2, 2)
+	m.SetValue(0, 0, 50.0)
+	m.SetValue(0, 1, 10.0)
+	m.SetValue(1, 1, 10.0)
+	m.SetValue(1, 0, 50.0)
+
+	if IsRowDominant(m) {
+		t.Error("Expected matrix not to be row dominant")
 	}
 }
